95fb029ec83886d06cac76c6dd7077de5acab936
rysn <imrays23@gmail.com>
2024-10-17 00:08:33 +0800

    modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import './Header.css'
+import "./Header.css";
+import Logo from "../assets/LGCVSU.png";
 
-const Header =() => {
-    return (
-      <div className="header">
-        <div className="logo">
-          <img src="../assets/pngwing.com.png" alt="" />
-        
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={Logo} alt="" />
+
         <h1>Cavite State University</h1>
         <div className="header-line"></div>
         <p>Student Portal</p>
-        </div>
       </div>
-
-      );
+    </div>
+  );
 };
 export default Header;
